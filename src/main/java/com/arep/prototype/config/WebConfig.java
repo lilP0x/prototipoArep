911f8ddc95d468e0e9f5f63c840c93459ece9516
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2025-05-16 08:18:07 -0500

    a

--- src/main/java/com/arep/prototype/config/WebConfig.java
+++ src/main/java/com/arep/prototype/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
